0dad8f92d0bb00774d2fb8cb0c25437f68f8ca23
shirejoni <hosseinshirjoni@chmail.ir>
2024-08-06 10:57:41 +0330

    fix log

--- 10-exam-two-actions/Transaction.ts
+++ 10-exam-two-actions/Transaction.ts
@@ -1,8 +1,9 @@
-import {map, timer} from "rxjs";
+import {map, tap, timer} from "rxjs";
 
 export class Transaction {
     Do() {
         return timer(3000).pipe(
+            tap(() => console.log("do transaction")),
             map(() => ({
                 balance: Math.random(),
                 status: "success"
